25a3fd0d7b54490dd08d18a595c02ba213f59bf2
go-evgn <stormdice23@gmail.com>
2022-05-04 12:27:54 +0500

    update

--- src/components/TripDays/TripDays.tsx
+++ src/components/TripDays/TripDays.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react";
+import { TripDay } from "../TripDay";
+import s from "./TripDays.module.css";
 
 export const TripDays: FC = () => {
-  return <ul className="trip-days"></ul>;
+  return (
+    <ul className={s["trip-days"]}>
+      <TripDay />
+    </ul>
+  );
 };
--- src/components/TripEvents/TripEvents.tsx
+++ src/components/TripEvents/TripEvents.tsx
@@ -6,20 +6,18 @@ import { TripDays } from "../TripDays";
 import { useGetPointsQuery } from "../../api/PointsApi";
 
 export const TripEvents: FC = () => {
-  const { addEventFormShowed: showed } = useAppSelector(
-    (state) => state.pointReducer
-  );
+  const { addEventFormShowed } = useAppSelector((state) => state.pointReducer);
   const { data, isLoading } = useGetPointsQuery("points");
 
   return (
     <section className={s.root}>
       <h2 className="visually-hidden">Trip events</h2>
 
-      {showed && <AddPointForm />}
-
-      {data && data.length > 0 && <TripDays />}
+      {addEventFormShowed && <AddPointForm />}
 
       {isLoading && <p className="trip-events__msg">Loading...</p>}
+
+      {data && data.length > 0 && <TripDays />}
 
       {data && data.length === 0 && (
         <p className="trip-events__msg">
